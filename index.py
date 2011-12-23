29ed501ce9a344226570613f22527db6b0a22d55
Christopher Swenson <cswenson@google.com>
2011-12-23 08:35:11 -0500

    Added MySQL support

--- index.py
+++ index.py
@@ -23,35 +23,53 @@ Stack Exchange.
 """
 
 import glob
+import MySQLdb
+import optparse
 import os
+import string
 import sys
-import sqlite3
 import time
 import xml.etree.cElementTree as et
+from collections import defaultdict
+
+WORDLETTERS = frozenset(string.ascii_lowercase + string.digits)
 
 z7 = '7za'  # You can overwrite this if you have a different 7zip binary.
 
-start_time = time.time()
+def delete_db(name):
+  conn = MySQLdb.connect(db='stackoverflow', use_unicode=True, charset='utf8') 
+  c = conn.cursor()
+  c.execute('delete from %s_comments;' % name)
+  c.execute('delete from %s_posts;' % name)
+  c.execute('delete from %s_users;' % name)
+  c.execute('drop table %s_comments;' % name)
+  c.execute('drop table %s_posts;' % name)
+  c.execute('drop table %s_users;' % name)
+  conn.commit()
+  c.close()
+  conn.close()
 
 def setup_db(name):
-  conn = sqlite3.connect(name)
+  conn = MySQLdb.connect(db='stackoverflow', use_unicode=True, charset='utf8') 
   c = conn.cursor()
-  c.execute('''create table users (
+  prefix = name
+  print name
+  c.execute('''create table %s_users (
     id integer(8) not null primary key,
     reputation integer(8),
     creationdate datetime,
-    displayname varchar(255),
+    displayname varchar(255) character set utf8,
     emailhash char(32),
     lastaccessdate datetime,
-    location varchar(255),
-    aboutme text,
+    location varchar(255) character set utf8,
+    aboutme text character set utf8,
     views integer(8),
     upvotes integer(8),
     downvotes integer(232),
     age integer(3),
-    websiteurl varchar(255)
-    )''')
-  c.execute('''create table posts (
+    websiteurl varchar(255) character set utf8
+    )''' % prefix)
+  c.execute('''create table %s_posts (
     id integer(8) not null primary key,
     parentid integer(8),
     posttypeid integer(8),
@@ -62,75 +80,80 @@ def setup_db(name):
     lastactivitydate datetime,
     owneruserid integer(8),
     lasteditoruserid integer(8),
-    lasteditordisplayname varchar(255),
+    lasteditordisplayname varchar(255) character set utf8,
     lasteditdate datetime,
     score integer(8),
-    viewcount integer(8),
-    title varchar(255),
-    body text,
-    tags varchar(255),
+    viewcount integer(8) default 0,
+    title varchar(255) character set utf8,
+    body text character set utf8,
+    tags varchar(255) character set utf8,
     answercount integer(8),
     commentcount integer(8),
     favoritecount integer(8)
-      )''')
-  c.execute('''create table comments (
+      )''' % prefix)
+  c.execute('''create fulltext index %s_posts_title on %s_posts (title)''' % (prefix, prefix))
+  c.execute('''create fulltext index %s_posts_body on %s_posts (body)''' % (prefix, prefix))
+  c.execute('''create fulltext index %s_posts_tags on %s_posts (tags)''' % (prefix, prefix))
+  c.execute('''create index %s_posts_parentid on %s_posts (parentid)''' % (prefix, prefix))
+  c.execute('''create index %s_posts_owneruserid on %s_posts (owneruserid)''' % (prefix, prefix))
+  c.execute('''create table %s_comments (
     id integer(8) not null primary key,
     postid integer(8) not null,
     score integer(8),
-    text text,
+    text text character set utf8,
     creationdate datetime,
     userid integer(8)
-  )''')
+  )''' % prefix)
+  c.execute('''create index %s_comments_postid on %s_comments (postid)''' % (prefix, prefix))
+  c.execute('''create index %s_comments_userid on %s_comments (userid)''' % (prefix, prefix))
+  c.execute('''create fulltext index %s_comments_text on %s_comments (text)''' % (prefix, prefix))
   conn.commit()
   return conn, c
 
-def create_users(cursor, users):
-  print "  Creating %d users" % len(users)
-  sys.stdout.write('  ')
-  i = 0
+def create_users(name, cursor, users):
+  print "  Creating users" 
+  table = "%s_users" % name
   for x in users:
-    if i & 0xfff == 0:
-      sys.stdout.write('.')
-      sys.stdout.flush()
-    i += 1
     keyvalues = [(k, x.get(k)) for k in x.keys()]
-    create(cursor, 'users', keyvalues)
-  sys.stdout.write('\n')
-  sys.stdout.flush()
+    create(cursor, table, keyvalues)
 
 def create(cursor, table, keyvalues):
   keys = [k for k, v in keyvalues]
   values = [v for k, v in keyvalues]
-  query = 'insert into %s (%s) values (%s);' % (table, ','.join(keys), ','.join(['?'] * len(values)))
+  query = 'insert into %s (%s) values (%s);' % (table, ','.join(keys), ','.join(['%s'] * len(values)))
   cursor.execute(query, values)
 
-def create_posts(cursor, posts):
-  print "  Creating %d posts" % len(posts)
-  sys.stdout.write('  ')
-  i = 0
+def create_posts(name, cursor, posts):
+  print "  Creating posts"
+  table = "%s_posts" % name
   for x in posts:
-    if i & 0xfff == 0:
-      sys.stdout.write('.')
-      sys.stdout.flush()
-    i += 1
     keyvalues = [(k, x.get(k)) for k in x.keys()]
-    create(cursor, 'posts', keyvalues)
-  sys.stdout.write('\n')
-  sys.stdout.flush()
+    create(cursor, table, keyvalues)
 
-def create_comments(cursor, comments):
-  print "  Creating %d comments" % len(comments)
-  sys.stdout.write('  ')
-  i = 0
+def create_comments(name, cursor, comments):
+  print "  Creating comments"
+  table = "%s_comments" % name
   for x in comments:
-    if i & 0xfff == 0:
-      sys.stdout.write('.')
-      sys.stdout.flush()
-    i += 1
     keyvalues = [(k, x.get(k)) for k in x.keys()]
-    create(cursor, 'comments', keyvalues)
-  sys.stdout.write('\n')
-  sys.stdout.flush()
+    create(cursor, table, keyvalues)
+
+def stripout(word):
+  return ''.join(x for x in word if x in WORDLETTERS)
+
+def get_words(text):
+  if not text:
+    return []
+  words = text.lower().strip().split()
+  words = [stripout(w) for w in words]
+  words = set(w for w in words if w)
+  return sorted(words)
+
+def get_tags(post):
+  tags = post.get('Tags')
+  if not tags:
+    return []
+  tags = tags[1:-1].split('><')
+  return sorted(set(t for t in tags if t))
 
 def check_dir(directory):
   if os.path.exists(os.path.join(directory, 'posts.xml')) and \
@@ -139,36 +162,77 @@ def check_dir(directory):
     return True
   return False
 
+def db_exists(dbname):
+  conn = MySQLdb.connect(db='stackoverflow', use_unicode=True, charset='utf8') 
+  c = conn.cursor()
+  c.execute('show tables;')
+  row = c.fetchone()
+  while row is not None:
+    table_name = row[0]
+    if unicode(dbname) == table_name.split('_')[0]:
+      return True
+    row = c.fetchone() 
+  return False
 
-for z in glob.glob('*.7z') + glob.glob('*.7z.001'):
-  os.system('echo s | ' + z7 + ' x ' + z)
+def parse(f):
+  for line in open(f):
+    l = line.strip()
+    if not l.startswith('<row'): continue
+    yield et.fromstring(l)
 
-posts = glob.glob('**/posts.xml')
-dirs = set(x[:-10] for x in posts)
-for directory in dirs:
-  dir_start_time = time.time()
-  check_dir(directory)
-  dbname = ''.join(directory.lower().split(' ')[1:]) + '.sqlite3'
-  print "Processing [%s] => %s" % (directory, dbname)
-  if os.path.exists(dbname): 
-    print '  Database exists... skipping.'
-    continue
-  posts_file, users_file, comments_file = [os.path.join(directory, x) for x in ('posts.xml', 'users.xml', 'comments.xml')]
-  users = et.parse(users_file).getroot()
-  conn, cursor = setup_db(dbname)
-  create_users(cursor, users)
-  del users
-  conn.commit()
-  posts = et.parse(posts_file).getroot()
-  create_posts(cursor, posts)
-  del posts
-  conn.commit()
-  comments = et.parse(comments_file).getroot()
-  create_comments(cursor, comments)
-  del comments
-  conn.commit()
-  cursor.close()
-  dir_end_time = time.time()
-  print '  Time: %.f seconds' % (dir_end_time - dir_start_time)
-end_time = time.time()
-print "Total time: %.f seconds" % (end_time - start_time)
+def main(options):
+  start_time = time.time()
+  if options.unzip:
+    for z in glob.glob('*.7z') + glob.glob('*.7z.001'):
+      os.system('echo s | ' + z7 + ' x ' + z)
+  
+  posts = glob.glob('**/posts.xml')
+  dirs = set(x[:-10] for x in posts)
+  for directory in dirs:
+    dir_start_time = time.time()
+    check_dir(directory)
+    dbname = ''.join(directory.lower().split(' ')[1:]) 
+    dbname = dbname.replace('-', '')
+    print "Processing [%s] => %s" % (directory, dbname)
+    do_db = True 
+    if db_exists(dbname):
+      if options.delete:
+        print 'Deleting previous entries'
+        delete_db(dbname)
+      else:
+        print '  Database exists... skipping.'
+        do_db = False
+    posts_file, users_file, comments_file = [os.path.join(directory, x) for x in ('posts.xml', 'users.xml', 'comments.xml')]
+    users = parse(users_file)
+    if options.mysql and do_db:
+      users = parse(users_file)
+      conn, cursor = setup_db(dbname)
+      create_users(dbname, cursor, users)
+      conn.commit()
+    posts = parse(posts_file)
+    if options.mysql and do_db:
+      posts = parse(posts_file)
+      create_posts(dbname, cursor, posts)
+      conn.commit()
+    comments = parse(comments_file)
+    if options.mysql and do_db:
+      comments = parse(comments_file)
+      create_comments(dbname, cursor, comments)
+      conn.commit()
+      cursor.close()
+    dir_end_time = time.time()
+    print '  Time: %.f seconds' % (dir_end_time - dir_start_time)
+  end_time = time.time()
+  print "Total time: %.f seconds" % (end_time - start_time)
+
+if __name__ == '__main__':
+  option_parser = optparse.OptionParser()
+  option_parser.add_option('-z', '--unzip', default=False, action='store_true',
+                           help='Unzip the .7z files')
+  option_parser.add_option('-d', '--delete', default=False, action='store_true',
+                           help='Delete any existing tables of the same names')
+  option_parser.add_option('-M', '--no-mysql', default=True, action='store_false',
+                           dest='mysql', help='Don\'t write to MySQL')
+
+  options, args = option_parser.parse_args()
+  main(options)
--- server.rb
+++ server.rb
@@ -15,6 +15,7 @@
 
 # This is the main server that serves the Stack Exchange data dumps.
 
+require 'date'
 require 'rubygems'
 
 # Load vendor/*
@@ -22,32 +23,70 @@ Dir["#{File.dirname(__FILE__)}/vendor/**"].each { |dir|
   $LOAD_PATH.unshift dir + '/lib'
 }
 
+require 'nokogiri'
 require 'sinatra'
 require 'sequel'
 
-# Load databases
-dbs = {}
-Dir["#{File.dirname(__FILE__)}/*.sqlite3"].each { |dbfile|
-  dbfile = dbfile.sub(/\.\//, '')
-  puts dbfile
-  db = Sequel.connect("sqlite://#{dbfile}")
-  dbname = dbfile.sub(/\.sqlite3/, '')
-  dbs[dbname] = db
-}
+# Load tables
 
+module DBHOLDER
+  @@db = nil
+
+  def self.set(x)
+    @@db = x
+  end
+
+  def self.get
+    @@db
+  end
+end
+
+require 'logger'
+
+DBHOLDER::set Sequel.connect("mysql://localhost/stackoverflow", :user => 'swenson', :socket => '/var/run/mysqld/mysqld.sock', :loggers => [Logger.new($stdout)])
 
 layout 'layout'
 set :public_folder, File.dirname(__FILE__) + '/static'
 
-helpers do
+dbs = DBHOLDER::get().tables.map { |x| x.to_s }.select { |x| x.end_with? 'posts' }.map { |x| x.sub(/_posts/, '') }
+
+helpers do  
+  def get_random_article_of_the_day(site)
+    srand(Date.today.hash)
+    count = find_posts(site).where(:score > 25).count()
+    find_posts(site).where(:score > 25).limit(1, rand(count)).first(1)[0]
+  end
+  
+  def make_snippet(post)
+    doc = Nokogiri::HTML(post[:body])
+    doc.xpath("//text()").remove.text[0,100]
+  end
+  
+  def get_tag_cloud(post)
+     tags_text = post[:tags]
+     tags = tags_text[1,tags_text.size - 2].split('><')
+     tags.sort.uniq.map { |tag| taglink(tag) }.join(' ')
+  end
+  
+  def get_site_tag_cloud(site)
+    tags_text = find_posts(site).first(25).map { |post| post[:tags] }.join
+    tags = tags_text[1,tags_text.size - 2].split('><')
+    tags.sort.uniq.map { |tag| taglink(tag) }.join(' ')
+  end
+  
   def find_user_by_id(x)
-    users = @db[:users].where(:id => x).first(1)
+    users = find_users(@site).where(:id => x).first(1)
     if users.empty?
       nil
     else
       users[0]
     end
   end
+  
+  def taglink(x)
+    "<a href=\"/#{@site}/tag/#{x}\">#{x}</a>"
+  end
+  
   def userlink(x)
     user = find_user_by_id(x)
     if user.nil?
@@ -56,8 +95,21 @@ helpers do
       "<a href=\"/#{@site}/user/#{x}\">#{user[:displayname]}</a>"
     end
   end
+  
   def postlink(x)
     "/#{@site}/post/#{x}"
+  end
+  
+  def find_posts(site)
+    @db["#{site}_posts".to_sym]
+  end
+  
+  def find_users(site)
+    @db["#{site}_users".to_sym]
+  end
+  
+  def find_comments(site)
+    @db["#{site}_comments".to_sym]
   end
 end
 
@@ -66,11 +118,15 @@ before do
   cache_control :public, :max_age => 3600
 end
 
-before '/:site/*' do
+before '/:site/?*' do
   @title = ''
   @site = params[:site]
   @action = "/#{@site}/search"
-  @db = dbs[params[:site]] if dbs.include?(params[:site])
+  @db = DBHOLDER::get
+end
+
+get '/favicon.ico' do
+  halt(404)
 end
 
 get '/' do
@@ -78,33 +134,47 @@ get '/' do
   @action = '/stackoverflow/search'
   @title = 'Stack Exchange Mirror'
   @names = dbs.map { |name, _| name }.sort
+  @tags = ''
   erb :index
 end
 
 get '/:site/?' do
+  @tags = get_site_tag_cloud(@site)
+  @random = get_random_article_of_the_day(@site)
   erb :site
 end
 
 get '/:site/search/?' do
   @query = params[:q]
-  @posts = @db[:posts].filter(:body.like("%#{@query}%")).order(:score).reverse.first(25)
+  @posts = find_posts(@site).filter(:body.like("%#{@query}%")).order(:score).reverse.first(25)
+  @tags = get_site_tag_cloud(@site)
+  erb :search
+end
+
+get '/:site/tag/:tag' do
+  @tag = params[:tag]
+  @query = "tag:#{@tag}"
+  @tags = get_site_tag_cloud(@site)
+  @posts = find_posts(@site).filter(:tags.like("%#{@tag}%")).order(:score).reverse.first(25)
   erb :search
 end
 
 get '/:site/user/:id' do
   @user_keys = [:displayname, :reputation, :age, :location, :upvotes, :downvotes, :views, :creationdate, :lastaccessdate, :aboutme, :websiteurl]
   @user = find_user_by_id(params[:id])
+  @tags = get_site_tag_cloud(@site)
   halt(404) if @user.nil?
   erb :user
 end
 
 get '/:site/post/:id' do
-  post = @db[:posts].where(:id => params[:id]).first(1)
+  post = find_posts(@site).where(:id => params[:id]).first(1)
   halt(404) if post.empty?
   @post = post[0]
-  @answers = @db[:posts].where(:parentid => params[:id]).all
+  @tags = get_tag_cloud(@post)
+  @answers = find_posts(@site).where(:parentid => params[:id]).all
   @accepted = @post[:acceptedanswerid] or -1
-  @comments = @db[:comments].where(:postid => params[:id]).all
+  @comments = find_comments(@site).where(:postid => params[:id]).all
   @comments = @comments.sort_by { |comment| -(comment[:score] or 0) }
   erb :post
 end
